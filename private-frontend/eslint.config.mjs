235403ddaf3f3f6811c3b32cf183df638bbe4727
Julien Hery <julien_hery@hotmail.fr>
2025-02-21 16:53:31 +0100

    fix: correctly setup npm commands
    Some commands were incorrect and were not working

--- private-frontend/eslint.config.mjs
+++ private-frontend/eslint.config.mjs
@@ -10,5 +10,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
 ];
